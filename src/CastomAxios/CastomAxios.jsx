24a59711a504f4d4ea9ddf9fc931427a3134e847
farrokhdev <farrokhdevs@gmail.com>
2023-06-26 13:28:46 +0330

    updated

--- src/CastomAxios/CastomAxios.jsx
+++ src/CastomAxios/CastomAxios.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const castomAxios = axios.create({
-  baseURL: "http://localhost:5001/",
+  // baseURL: "http://localhost:5001/",
+  baseURL: "https://socialapp-server.onrender.com/",
   headers: { "Content-Type": "application/json" },
 });
